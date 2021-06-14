533958a385a8fac6f5c3270db3dcc420e6457896
aimless-cpu <s.grabher@protonmail.com>
2021-06-14 20:59:14 +0200

    added 4 menupoints, point 2 working

--- src/at/dom/box/gui/ConsoleGUI.java
+++ src/at/dom/box/gui/ConsoleGUI.java
@@ -2,36 +2,64 @@ package at.dom.box.gui;
 
 import at.dom.box.main.MusicBox;
 
+import java.util.Scanner;
+
 public class ConsoleGUI implements GUI{
     private MusicBox musicBox;
 
     @Override
     public void start() {
+        int input;
+        String searchString;
+
+        System.out.println("1 search for record");
+        System.out.println("2 get sum of music");
+        System.out.println("3 load a record");
+        System.out.println("4 play a record");
 
 
-        System.out.println();
-        System.out.println("suche nach dem at.dom.box.main.Song moenytalks");
-        musicBox.searchRecord("moneytalks");
+        Scanner scanner = new Scanner(System.in);
 
-        musicBox.ausgabeRecordList();
+        input = scanner.nextInt();
 
-        System.out.println("Ausgbe Länge alles titel und records");
+        if (input == 1) {
+            System.out.println("Write the name of the record your looking for:");
+            searchString = scanner.next();
+            musicBox.searchRecord(searchString);
+        }
+        if (input == 2) {
+            System.out.println(musicBox.getSumOfMusic());
+        }
+        if (input == 3) {
+            //System.out.println(musicBox.ausgabeRecordList());
+        }
 
-        System.out.println(musicBox.getSumOfMusic());
-
-        //musicBox.removeRecord(recordACDC);
-
-        musicBox.ausgabeRecordList();
 
 
-        //musicBox.loadRecords(recordACDC);
-        musicBox.load(4);
+//
+//        System.out.println();
+//        System.out.println("suche nach dem at.dom.box.main.Song moenytalks");
+//        musicBox.searchRecord("moneytalks");
+//
+//        musicBox.ausgabeRecordList();
+//
+//        System.out.println("Ausgbe Länge alles titel und records");
+//
+//        System.out.println(musicBox.getSumOfMusic());
+//
+//        //musicBox.removeRecord(recordACDC);
+//
+//        musicBox.ausgabeRecordList();
+//
+//
+//        //musicBox.loadRecords(recordACDC);
+//        musicBox.load(4);
 
-        //test
-        System.out.println("dominik");
-
-        System.out.println("hello i am stefan");
-        System.out.println("second");
+//        //test
+//        System.out.println("dominik");
+//
+//        System.out.println("hello i am stefan");
+//        System.out.println("second");
     }
 
     @Override
